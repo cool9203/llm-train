4b3d01070df02e3b4488b276f3fe1775a7f03602
cool9203 <octer18@gmail.com>
2025-05-21 15:12:13 +0800

    Update: qwen25-sft.sh

--- scripts/qwen25-sft.sh
+++ scripts/qwen25-sft.sh
@@ -1,20 +1,25 @@
 accelerate launch src/llm_train/train_vlm.py \
+	`# Dataset` \
 	--dataset_name data/tmpco-table-reasoning-code_block-row_comment-20250502-html.json \
+	--max_length 2048 \
+	`# Model` \
 	--model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
-	--per_device_train_batch_size 1 \
-	--gradient_accumulation_steps 4 \
-	--image_max_pixels 1631220 \
-	--output_dir saves/qwen2.5_vl-3b/tmpco/table/html/lora/sft/20250512 \
-	--logging_steps 10 \
-	--save_strategy epoch \
-	--bf16 \
-	--torch_dtype bfloat16 \
 	--attn_implementation flash_attention_2 \
-	--gradient_checkpointing \
+	--image_max_pixels 1631220 \
+	`# Lora` \
 	--use_peft \
 	--lora_dropout 0.0 \
 	--lora_target_modules ".*proj" \
-	--max_length 2048 \
+	`# Output` \
+	--output_dir saves/qwen2.5_vl-3b/tmpco/table/html/lora/sft/20250512 \
+	--logging_steps 10 \
+	--save_strategy epoch \
+	`# Train` \
+	--per_device_train_batch_size 1 \
+	--gradient_accumulation_steps 4 \
+	--bf16 \
+	--torch_dtype bfloat16 \
+	--gradient_checkpointing \
 	--learning_rate 1.0e-4 \
 	--num_train_epochs 10.0 \
 	--lr_scheduler_type cosine \
