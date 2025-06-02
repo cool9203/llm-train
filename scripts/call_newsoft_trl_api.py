e4086f488858e40225a48097a433ce11446dbb79
cool9203 <octer18@gmail.com>
2025-06-02 14:51:14 +0800

    Update: rewrite

--- scripts/call_newsoft_trl_api.py
+++ scripts/call_newsoft_trl_api.py
@@ -1,99 +1,91 @@
 # coding: utf-8
 
+import argparse
 import ast
 import json
 import os
 import re
+from pathlib import Path
 
 import requests
 from tqdm import tqdm
 
-# folder_path = "/mnt/e/力新AIOCR/整張單據/100張的完整單據_VLM-4-Testing"  # 請替換為實際的資料夾路徑
-folder_path = "/home/localadmin/data_center/100張的完整單據_VLM-4-Testing"
-# folder_path = "/mnt/e/力新AIOCR/整張單據/20250416_測試803張"
 
-# url = "http://10.70.0.128:30303/api/inference_table"
-# url = "http://10.70.0.232:7861/api/inference_table"
-url = "http://127.0.0.1:7866/api/inference_table"
-# output_json = 'yoga_5005pic_6000steps_v4_api_test803pics_result.json'
-# output_json = 'trl_test100pics_3b_0526_max_pixel_result.json'
-output_json = "trl_test100pics_3b_0527_ck-4455_result.json"
-max_tokens = 512
-# prompt = '請從下列發票圖像中擷取以下欄位資訊，並以 JSON 格式回傳：InvoiceNumber、CompanyName、BuUniformNumber、InvoiceDate、NetAmount、TaxAmount、TotalAmount、VnUniformNumber'
-prompt = "請從下列發票圖像中擷取以下欄位資訊，並以 JSON 格式回傳：InvoiceNumber、CompanyName、BuUniformNumber、InvoiceDate、NetAmount、TaxAmount、TotalAmount、TotalAmountCH、VnUniformNumber。"
-mapping_name = {
-    "發票號碼": "InvoiceNumber",
-    "買受人名稱": "CompanyName",
-    "賣方公司統編": "VnUniformNumber",
-    "買方公司統編": "BuUniformNumber",
-    "發票日期": "InvoiceDate",
-    # "品項": "item",
-    "未稅金額": "NetAmount",
-    "稅額": "TaxAmount",
-    "總金額": "TotalAmount",
-    "手寫金額": "TotalAmountCH",
-}
+def arg_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Run NewSoft invoice result")
+    parser.add_argument("-i", "--input_path", type=str, required=True, help="Input data path")
+    parser.add_argument("-o", "--output", type=str, required=True, help="Output result filename")
+    parser.add_argument("--api_url", type=str, required=True, help="Api url")
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        default="請從下列發票圖像中擷取以下欄位資訊，並以 JSON 格式回傳：InvoiceNumber、CompanyName、BuUniformNumber、InvoiceDate、NetAmount、TaxAmount、TotalAmount、TotalAmountCH、VnUniformNumber。",
+        help="Api parameter: prompt",
+    )
+    parser.add_argument("--max_token", type=int, default=512, help="Api parameter: max token")
 
-# 在处理图片前，先检查是否存在之前的结果
-existing_results = []
-if os.path.exists(output_json):
-    try:
-        with open(output_json, "r", encoding="utf-8") as json_file:
-            existing_results = json.load(json_file)
-    except Exception:
-        existing_results = []
+    args = parser.parse_args()
 
-# 获取已处理的文件名列表
-processed_files = set(item["filename"] for item in existing_results)
-all_result = existing_results  # 使用已存在的结果作为起点
+    return args
 
-# 獲取所有圖片文件
-image_files = []
-for root, dirs, files in os.walk(folder_path):
-    for file in files:
-        if file.lower().endswith((".jpg", ".png", ".jpeg")):
-            image_files.append(os.path.join(root, file))
 
-# 使用tqdm创建进度条，但只显示未处理的文件
-unprocessed_images = [img for img in image_files if os.path.basename(img) not in processed_files]
-for image_path in tqdm(unprocessed_images, desc="處理圖片中"):
-    with open(image_path, "rb") as image_file:
-        files = {"image": image_file}
-        # data = {
-        #     'detect_table': False,
-        #     'repair_latex': False,
-        #     'full_border': False,
-        #     'unsqueeze': False,
-        #     'system_prompt':None,
-        #     # 'default_prompt':
-        #     # 'prompt': '擷取該發票的資訊並輸出成JSON格式'
-        #     'prompt':prompt
-        # }
-        data = {"model_name": "lora", "img_type": "png", "system_prompt": None, "max_tokens": max_tokens, "prompt": prompt}
-        response = requests.post(url, files=files, data=data)
-        response = response.json()
-        if "images" in response:
-            del response["images"]
-        print(response)
-        origin_content = response["origin_content"]
-        origin_content = re.sub(r'"品項"\s*:\s*\[.*?\],?', "", origin_content, flags=re.DOTALL)
+def call_newsoft_trl_api(
+    api_url: str,
+    input_path: os.PathLike,
+    output_path: os.PathLike,
+    prompt: str,
+    max_tokens: int = 512,
+):
+    # 處理圖片前, 先取得過往已處理的資料
+    existing_results = list()
+    if Path(output_path).exists():
         try:
-            # origin_content = json.loads(origin_content)
-            origin_content = ast.literal_eval(origin_content)
-        except Exception as e:
-            print(e)
-            pass
-        # 根据mapping_name替换键名
-        mapped_content = {}
-        for zh_key, en_key in mapping_name.items():
-            # print(zh_key, en_key)
-            if zh_key in origin_content:
-                mapped_content[en_key] = origin_content[zh_key]
-            elif en_key in origin_content:
-                mapped_content[en_key] = origin_content[en_key]
-        # print(mapped_content)
-        all_result.append(
-            {"filename": os.path.basename(image_path), "api_result": mapped_content, "used_time": response["used_time"]}
-        )
-        with open(output_json, "w", encoding="utf-8") as json_file:
-            json.dump(all_result, json_file, ensure_ascii=False, indent=4)
+            with open(output_path, "r", encoding="utf-8") as json_file:
+                existing_results = json.load(json_file)
+        except Exception:
+            existing_results = list()
+
+    # 取得已處理的檔名
+    processed_files = set(item["filename"] for item in existing_results)
+    all_result = existing_results  # 使用已存在的结果作为起点
+
+    # 獲取所有圖片文件
+    image_files = []
+    for root, dirs, files in os.walk(input_path):
+        for file in files:
+            if file.lower().endswith((".jpg", ".png", ".jpeg")):
+                image_files.append(os.path.join(root, file))
+
+    # Run
+    unprocessed_images = [img for img in image_files if os.path.basename(img) not in processed_files]
+    for image_path in tqdm(unprocessed_images, desc="處理圖片中"):
+        with open(image_path, "rb") as image_file:
+            files = {"image": image_file}
+            data = {
+                "model_name": "lora",
+                "img_type": "png",
+                "system_prompt": None,
+                "max_tokens": max_tokens,
+                "prompt": prompt,
+            }
+            response = requests.post(api_url, files=files, data=data)
+            response = response.json()
+            if "images" in response:
+                del response["images"]
+            print(response)
+            origin_content = str(response["origin_content"]).lower()
+            origin_content = re.sub(r'"品項"\s*:\s*\[.*?\],?', "", origin_content, flags=re.DOTALL)
+            try:
+                origin_content = ast.literal_eval(origin_content)
+            except Exception as e:
+                print(e)
+
+            all_result.append(
+                {
+                    "filename": os.path.basename(image_path),
+                    "api_result": origin_content,
+                    "used_time": response["used_time"],
+                }
+            )
+            with Path(Path(output_path).stem + ".json").open(mode="w", encoding="utf-8") as json_file:
+                json.dump(all_result, json_file, ensure_ascii=False, indent=4)
--- scripts/eval/v4_compare_newsoft_with_ans.py
+++ scripts/eval/v4_compare_newsoft_with_ans.py
@@ -1,4 +1,9 @@
+# coding: utf-8
+
+import argparse
 import json
+import os
+from pathlib import Path
 
 import pandas as pd
 
@@ -9,90 +14,86 @@ try:
 except ImportError:
     support_cn2an = False
 
-print(f"是否支援中文數字轉換: {support_cn2an}")
+
+def arg_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Compare NewSoft invoice correct")
+    parser.add_argument("-i", "--input_path", type=str, required=True, help="Input data path")
+    parser.add_argument("-o", "--output", type=str, required=True, help="Output result filename")
+    parser.add_argument("--answer_path", type=str, required=True, help="Answer data path")
+
+    args = parser.parse_args()
+
+    return args
 
 
-def get_json_result(json_input_path):
-    with open(json_input_path, "r", encoding="utf-8") as f:
-        return json.load(f)
+def v4_compare_with_ans(
+    answer_path: os.PathLike,
+    input_path: os.PathLike,
+    output_path: os.PathLike,
+):
+    if Path(answer_path).suffix == ".xlsx":
+        df = pd.read_excel(answer_path)
+    elif hasattr(pd, f"read_{Path(answer_path).suffix[1:]}"):
+        df = getattr(pd, f"read_{Path(answer_path).suffix[1:]}")(
+            answer_path,
+            encoding="utf-8",
+            dtype=str,
+        )
+    else:
+        raise ValueError(f"Not support read '{Path(answer_path).suffix}'")
 
+    df.columns = df.columns.str.lower()
+    with Path(input_path).open(mode="r", encoding="utf-8") as f:
+        api_result = json.load(f)
 
-def v4_compare_with_ans(ans_csv_file, api_result_path, compare_output_path):
-    df = pd.read_csv(ans_csv_file, encoding="utf-8", dtype=str)
-    api_result = get_json_result(api_result_path)
-    # results = [{'filename': '_1212_231227085500552_007_9C82033B.jpg', 'api_result': {'result': {'InvoiceNumber': {'value': 'TY37092609'}, 'CompanyName': {'value': '旭聖光電企業有限公司'}, 'VnUniformNumber': {'value': '84172879'}, 'BuUniformNumber': {'value': '28752380'}, 'InvoiceDate': {'value': '2023-11-01'}, 'ItemName': [{'value': 'AP2F90V0059'}], 'NetAmount': {'value': '600'}, 'TaxAmount': {'value': '300'}, 'TotalAmount': {'value': '6300'}, 'TotalAmountCH': {'value': '陸仟叁佰元整'}}, 'processing_time': '4.76s', 'id': 'b2b64dc97c12450555b4ebb51ef2e61e'}}, {'filename': '_1212_231227085501301_008_F5C749F9.jpg', 'api_result': {'result': {'InvoiceNumber': {'value': 'TY37092615'}, 'CompanyName': {'value': '旭望光電企業有限公司'}, 'VnUniformNumber': {'value': '84172879'}, 'BuUniformNumber': {'value': '28752380'}, 'InvoiceDate': {'value': '2023-11-01'}, 'ItemName': [{'value': 'AP-20-FD02940+RL+T1'}, {'value': 'AP-20-FD02940+RL+DEC'}, {'value': 'AP-20-FD02940+POS+RL+T1'}], 'NetAmount': {'value': '39660'}, 'TaxAmount': {'value': '1983'}, 'TotalAmount': {'value': '41643'}, 'TotalAmountCH': {'value': ' 肆萬壹仟陸佰肆拾叁元'}}, 'processing_time': '6.19s', 'id': 'efa92a1e597cfe3a1a7139cc9f12e42f'}}, {'filename': '_1212_231227085502260_009_C90378AC.jpg', 'api_result': {'result': {'InvoiceNumber': {'value': 'TY37092607'}, 'CompanyName': {'value': '旭壓光電企業有限公司'}, 'VnUniformNumber': {'value': '84172879'}, 'BuUniformNumber': {'value': '28752380'}, 'InvoiceDate': {'value': '2023-11-01'}, 'ItemName': [{'value': '電路板'}], 'NetAmount': {'value': '4500'}, 'TaxAmount': {'value': '225'}, 'TotalAmount': {'value': '4725'}, 'TotalAmountCH': {'value': '肆仟柒佰贰拾伍元'}}, 'processing_time': '4.64s', 'id': 'f5088d04806931ff5bf12b2023704cdc'}}]
-    # api_result = [{
-    #     "filename": "_1212_231227085502260_009_C90378AC.jpg",
-    #     "api_result": {
-    #         "InvoiceNumber": "TY37092607",
-    #         "CompanyName": "協麟光電企業有限公司",
-    #         "VnUniformNumber": "84172879",
-    #         "BuUniformNumber": "28752380",
-    #         "InvoiceDate": "中華民國112年11月1日",
-    #         "item": [
-    #             "電路板"
-    #         ],
-    #         "NetAmount": "4500",
-    #         "TaxAmount": "225",
-    #         "TotalAmount": "4725",
-    #         "TotalAmountCH": "肆仟柒佰貳拾伍元"
-    #     },
-    #     "used_time": 6.782865524291992
-    # }]
-    # 跟正確答案比較
-    global InvoiceNumber_counter
-    InvoiceNumber_counter = 0
-    global CompanyName_counter
-    CompanyName_counter = 0
-    global VnUniformNumber_counter
-    VnUniformNumber_counter = 0
-    global BuUniformNumber_counter
-    BuUniformNumber_counter = 0
-    global InvoiceDate_counter
-    InvoiceDate_counter = 0
-    global NetAmount_counter
-    NetAmount_counter = 0
-    global TaxAmount_counter
-    TaxAmount_counter = 0
-    global TotalAmount_counter
-    TotalAmount_counter = 0
-    global TotalAmountCH_counter
-    TotalAmountCH_counter = 0
+    correct_counter = dict(
+        InvoiceNumber=0,
+        CompanyName=0,
+        VnUniformNumber=0,
+        BuUniformNumber=0,
+        InvoiceDate=0,
+        NetAmount=0,
+        TaxAmount=0,
+        TotalAmount=0,
+        TotalAmountCH=0,
+    )
+    correct_counter = {k.lower(): dict(name=k, count=v) for k, v in correct_counter.items()}
+
+    print(f"是否支援中文數字轉換: {support_cn2an}")
 
     all_ary = []
     for item in api_result:
         single_dict = {}
         filename = item["filename"]
-        # item_ocr_result =  item['api_result']['result']
         item_ocr_result = item["api_result"]
+        item_ocr_result = {k.lower(): v for k, v in item["api_result"].items()}
         matched_row = df[df["filename"] == filename]
         for match_row_key in matched_row:
+            match_row_key = match_row_key.lower()
             if match_row_key == "filename":
                 single_dict[match_row_key] = filename
                 continue
-            # print(match_row_key, matched_row[match_row_key])
-            # print(item_ocr_result[match_row_key]['value'], matched_row[match_row_key].iloc[0])
-            # if filename not in all_dict:
-            #     all_dict[filename] = {}
             if match_row_key in item_ocr_result:
                 single_dict[f"{match_row_key}_iii"] = str(item_ocr_result[match_row_key])
                 single_dict[f"{match_row_key}_ans"] = str(matched_row[match_row_key].iloc[0])
 
                 if str(item_ocr_result[match_row_key]) == str(matched_row[match_row_key].iloc[0]):
-                    globals()[f"{match_row_key}_counter"] += 1
+                    correct_counter[match_row_key]["count"] += 1
                     single_dict[f"{match_row_key}_result"] = 1
                 else:
-                    if match_row_key == "InvoiceDate":
+                    if match_row_key == "InvoiceDate".lower():
                         if str(item_ocr_result[match_row_key]) == f"中華民國{str(matched_row[match_row_key].iloc[0])}":
-                            globals()[f"{match_row_key}_counter"] += 1
+                            correct_counter[match_row_key]["count"] += 1
                             single_dict[f"{match_row_key}_result"] = 1
                             continue
-                    if match_row_key == "TotalAmountCH" and support_cn2an:
+                    if match_row_key == "TotalAmountCH".lower() and support_cn2an:
                         try:
                             if cn2an.cn2an(
-                                str(item_ocr_result[match_row_key]).replace("參", "叁").replace("兩", "二")
-                            ) == cn2an.cn2an(str(matched_row[match_row_key].iloc[0]).replace("參", "叁").replace("兩", "二")):
-                                globals()[f"{match_row_key}_counter"] += 1
+                                str(item_ocr_result[match_row_key]).replace("參", "叁").replace("兩", "二").replace("元", "")
+                            ) == cn2an.cn2an(
+                                str(matched_row[match_row_key].iloc[0]).replace("參", "叁").replace("兩", "二").replace("元", "")
+                            ):
+                                correct_counter[match_row_key]["count"] += 1
                                 single_dict[f"{match_row_key}_result"] = 1
                             continue
                         except ValueError:
@@ -100,57 +101,22 @@ def v4_compare_with_ans(ans_csv_file, api_result_path, compare_output_path):
 
                     single_dict[f"{match_row_key}_result"] = 0
             else:
-                # if filename not in all_dict:
-                #     all_dict[filename] = {}
                 print(match_row_key, item_ocr_result)
                 single_dict[f"{match_row_key}_iii"] = f"沒有這個key值: {match_row_key}"
                 single_dict[f"{match_row_key}_ans"] = str(matched_row[match_row_key].iloc[0])
                 single_dict[f"{match_row_key}_result"] = 0
         all_ary.append(single_dict)
-    # print(all_ary)
-    with open(f"{compare_output_path}.json", "w", encoding="utf-8") as json_file:
+
+    with Path(Path(output_path).stem + ".json").open(mode="w", encoding="utf-8") as json_file:
         json.dump(all_ary, json_file, ensure_ascii=False, indent=4)
-    print(
-        {
-            "InvoiceNumber_counter": InvoiceNumber_counter,
-            "CompanyName_counter": CompanyName_counter,
-            "VnUniformNumber_counter": VnUniformNumber_counter,
-            "BuUniformNumber_counter": BuUniformNumber_counter,
-            "InvoiceDate_counter": InvoiceDate_counter,
-            "NetAmount_counter": NetAmount_counter,
-            "TaxAmount_counter": TaxAmount_counter,
-            "TotalAmount_counter": TotalAmount_counter,
-            "TotalAmountCH_counter": TotalAmountCH_counter,
-        }
-    )
+    print({v["name"]: v["count"] for _, v in correct_counter.items()})
 
     # 將結果轉換為 DataFrame
     results_df = pd.DataFrame(all_ary)
-    results_df.to_csv(f"{compare_output_path}.csv", index=False, encoding="utf-8")
-    results_df.to_excel(f"{compare_output_path}.xlsx", index=False, engine="openpyxl")
-    # 比對結果
-    # comparison = pd.merge(df, results_df, on='filename', how='outer')
-
-    # 顯示比對結果
-    # print(comparison)
+    results_df.to_csv(str(Path(Path(output_path).stem + ".csv")), index=False, encoding="utf-8")
+    results_df.to_excel(Path(Path(output_path).stem + ".xlsx"), index=False, engine="openpyxl")
 
 
-ans_csv = "result_final.csv"
-# ans_csv = "newsoft_handwriting_20250416_new_format_remove_duplicate.csv"
-# api_result_path = "yoga_5005pic_6000steps_v4_api_result.json"
-# api_result_path = "yoga_5005pic_6000steps_v4_api_test803pics_result.json"
-# api_result_path = "pack_test_result.json"
-# api_result_path = "trl_test100pics_7b_max_pixel_result.json"
-# api_result_path = "trl_test100pics_3b_0526_result.json"
-# api_result_path = "trl_test100pics_3b_0527_max_pixel_ck-7425_result.json"
-api_result_path = "trl_test100pics_3b_0527_ck-4455_result.json"
-
-
-# compare_result_output = 'yoga_5005pic_6000steps_v4_compare_result'
-# compare_result_output = 'yoga_5005pic_6000steps_v4_api_test803pics_compare_result'
-# compare_result_output = 'pack_test_compare_result'
-# compare_result_output = 'trl_test100pics_7b_max_pixel_compare_result'
-compare_result_output = "trl_test100pics_3b_0527_ck-4455_compare_result"
-
-
-v4_compare_with_ans(ans_csv, api_result_path, compare_result_output)
+if __name__ == "__main__":
+    args = arg_parser()
+    v4_compare_with_ans(**vars(args))
