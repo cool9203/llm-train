a4d2632eafd2a42a623e7688fcbc9ab00ce16b17
cool9203 <octer18@gmail.com>
2025-06-03 14:08:07 +0800

    Add: merge lora script

---
+++ src/llm_train/merge_lora.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+import argparse
+
+from peft import PeftModel
+from transformers import (
+    AutoModel,
+    AutoProcessor,
+    PreTrainedModel,
+)
+
+
+def arg_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Merge lora to base model script")
+    parser.add_argument("-m", "--model_name", type=str, required=True, help="Merge base model name or path")
+    parser.add_argument("-o", "--output_path", type=str, required=True, help="Save path")
+    parser.add_argument("-l", "--lora_model", type=str, required=True, help="Lora adapter name or path")
+    parser.add_argument("--device_map", type=str, default="cuda:0", help="Device map, cuda or cpu")
+
+    args = parser.parse_args()
+
+    return args
+
+
+def merge_lora(
+    model_name: str,
+    output_path: str,
+    lora_model: list,
+    device_map: str = "cuda:0",
+):
+    base_model: PreTrainedModel = AutoModel.from_pretrained(
+        pretrained_model_name_or_path=model_name,
+        device_map=device_map,
+    )
+    merged_model = PeftModel.from_pretrained(base_model, lora_model)
+    tokenizer = AutoProcessor.from_pretrained(pretrained_model_name_or_path=model_name)
+
+    merged_model.merge_and_unload()
+
+    merged_model.save_pretrained(output_path)
+    tokenizer.save_pretrained(output_path)
+
+
+if __name__ == "__main__":
+    args = arg_parser()
+    merge_lora(**vars(args))
